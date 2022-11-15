3f55fff7d5b11f8a47da04ef018564debb3a42ea
BanupriyaMM <106953576+BanupriyaMM@users.noreply.github.com>
2022-11-15 16:48:29 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4a.java
@@ -0,0 +1,15 @@
+package activities;
+
+import org.testng.annotations.Test;
+
+public class Activity4a {
+    @Test
+    public void firstTestCase() {
+        System.out.println("This is the first test method");
+    }
+
+    @Test
+    public void secondTestCase() {
+        System.out.println("This is the second test method");
+    }
+}
---
+++ TestNG/Activities/Activity4b.java
@@ -0,0 +1,10 @@
+package activities;
+
+import org.testng.annotations.Test;
+
+public class Activity4b {
+    @Test
+    public void TestCase() {
+        System.out.println("This is the third test method");
+    }
+}
